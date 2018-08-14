aaeebeefb8db447cd8a0a39e36b01b3f18b1ff07
Nicholas Williams <nick5664@outlook.com>
2018-08-14 12:43:30 +0000

    anodda day

--- day10.cpp
+++ day10.cpp
@@ -1,37 +1,27 @@
-#include <cmath>
-#include <cstdio>
-#include <vector>
-#include <iostream>
-#include <algorithm>
-#include <map>
+#include <bits/stdc++.h>
+
 using namespace std;
 
 
-int main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
-    vector <string> queries;
-    map <string, int> phoneBook;
-    string tempQuery;
-    int numFrens;
-    cin>>numFrens;
-    for(int i; i<numFrens; i++){
-        string tempKey;
-        int tempVal;
-        cin>>tempKey>>tempVal;
-        phoneBook[tempKey] = tempVal;
-    }
-    while(getline(cin, tempQuery)){
-        if(tempQuery != ""){
-        queries.push_back(tempQuery);    
-        }
-    }
-    
-    for(size_t ix = 0; ix<queries.size(); ix++){
-        if(phoneBook.find(queries[ix]) == phoneBook.end()){
-            cout<<"Not found"<<endl;
+
+int main()
+{
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    bitset<32> binaryRep(n);
+    int in_a_row = 0;
+    int longest = 0;
+    for(size_t x = 0; x < 32; x++){
+        if(binaryRep[x] == 1){
+            in_a_row += 1;
         } else {
-            cout<<queries[ix]<<"="<<phoneBook[queries[ix]]<<endl;
+            if(in_a_row > longest){
+                longest = in_a_row;
+            }
+            in_a_row = 0;
         }
     }
+    cout<<longest<<endl;
     return 0;
 }
